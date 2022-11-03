47e3fc27b4129cfd81afafce4d7af770bccf4ff1
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-03 01:59:32 -0300

    Refatoração

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,42 +1,13 @@
-const buttonPom = document.querySelector(".tecla_pom");
-const buttonClap = document.querySelector(".tecla_clap");
-const buttonTim = document.querySelector(".tecla_tim");
-const buttonPuff = document.querySelector(".tecla_puff");
-const buttonSplash = document.querySelector(".tecla_splash");
-const buttonToim = document.querySelector(".tecla_toim");
-const buttonPsh = document.querySelector(".tecla_psh");
-const buttonTic = document.querySelector(".tecla_tic");
-const buttonTom = document.querySelector(".tecla_tom");
+const listaTeclas = document.querySelectorAll(".tecla");
+
+listaTeclas.forEach(tecla => {
+    tecla.addEventListener('click', ()=>{
+        const typeSong = tecla.textContent.toLocaleLowerCase();
+        playSong(typeSong);
+    });
+});
 
 function playSong(tipoSong){
     const song = document.querySelector(`#som_tecla_${tipoSong}`);
     return song.play()
 };
-
-buttonPom.addEventListener('click', () => {
-    playSong("pom")
-})
-buttonClap.addEventListener('click', () => {
-    playSong("clap")
-})
-buttonTim.addEventListener('click', () => {
-    playSong("tim")
-})
-buttonPuff.addEventListener('click', () => {
-    playSong("puff")
-})
-buttonSplash.addEventListener('click', () => {
-    playSong("splash")
-})
-buttonToim.addEventListener('click', () => {
-    playSong("toim")
-})
-buttonPsh.addEventListener('click', () => {
-    playSong("psh")
-})
-buttonTic.addEventListener('click', () => {
-    playSong("tic")
-})
-buttonTom.addEventListener('click', () => {
-    playSong("tom")
-})
